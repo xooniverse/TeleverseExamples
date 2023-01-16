ed12880c7d9af5824172df070a7c47282f54487d
Sreelal TS <heysreelal@gmail.com>
2023-01-16 23:55:56 +0530

    Counting documented! 👽

--- lib/letter_counter.dart
+++ lib/letter_counter.dart
@@ -2,19 +2,40 @@ import 'dart:io';
 
 import 'package:televerse/televerse.dart';
 
-Bot letterCounterBot = Bot(Platform.environment["BOT_TOKEN"]!);
+void main(List<String> args) {
+  // Create a new bot instance
+  Bot bot = Bot(Platform.environment["BOT_TOKEN"]!);
 
-Future<void> startLetterCounterBot() async {
-  letterCounterBot.command("start", (ctx) {
+  // The command method can be used to register a command handler
+  // You'll have to pass two arguments: the command name and the handler
+
+  // The handler is a function that takes a [MessageContext] as an argument
+  bot.command("start", (ctx) {
+    // The message context contains all the information about the incoming message
+    // and bunch of useful methods to reply to it.
+
     final String name = ctx.message.from!.firstName;
+
+    // The reply method can be used to send a message to the chat where the message
     ctx.reply("Hello $name!");
   });
 
-  letterCounterBot.on(TeleverseEvent.text, (ctx) {
+  /// The [on] method can be used to register a handler for a specific event
+  /// You'll have to pass two arguments: the event and the handler
+  ///
+  /// The handler is a function that takes a [Context] as an argument.
+  /// The context type depends on the event type. This is why we have to cast it.
+  bot.on(TeleverseEvent.text, (ctx) {
+    // Since we're handling a text message, we can cast the context to [MessageContext]
     ctx as MessageContext;
+
+    // We can get the message text and count the letters
     final int letterCount = ctx.message.text!.length;
+
+    // We can reply to the message
     ctx.reply("Your message has $letterCount letters.");
   });
 
-  letterCounterBot.start();
+  // Start the bot
+  bot.start();
 }
