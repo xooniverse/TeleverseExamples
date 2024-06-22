1c30d889d3a0b1d7bdceaad48d6cfd88208e87be
Sreelal TS <heysreelal@gmail.com>
2024-06-22 23:55:50 +0530

    ℹ️ Middleware & Transformer Examples

---
+++ lib/middleware_example.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+import 'package:televerse/televerse.dart';
+
+final bot = Bot(Platform.environment["BOT_TOKEN"]!);
+
+// Create a middleware or import one if already built
+
+class Logger implements Middleware {
+  // The `fn` is the middleware function that will be excuted over the context.
+  // Calling the `next` function will execute the following middleware or the main handler.
+  @override
+  void fn(Context ctx, NextFunction next) {
+    // Suppose we're just printing the update id
+    print(ctx.update.updateId);
+
+    // Move to the next handler, if you skip this invocation, the
+    // middleware chain is ended here.
+    next();
+  }
+}
+
+void main(List<String> args) {
+  // Attach the middleware to the bot
+  bot.use(Logger());
+
+  // Continue setting up your bot
+
+  bot.command('start', (ctx) async {
+    await ctx.reply("Hello World");
+  });
+
+  bot.help((ctx) async {
+    await ctx.reply("Here to help!");
+  });
+
+  // Now on all succeeding handler calls, the update id is logged.
+
+  bot.start();
+}
---
+++ lib/transformer_example.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+import 'package:televerse/telegram.dart';
+import 'package:televerse/televerse.dart';
+
+final bot = Bot(Platform.environment["BOT_TOKEN"]!);
+
+// Create a Transformer or import one if already built
+
+class AutoReplier implements Transformer {
+  @override
+  Map<String, dynamic> fn(
+    APIMethod method,
+    Map<String, dynamic> payload,
+    Context? ctx,
+  ) {
+    // We'll force the user to reply to the message bot sends
+
+    final isSendMethod = APIMethod.sendMethods.contains(method);
+    final isNotChatAction = method != APIMethod.sendChatAction;
+
+    // If bot is calling a `send` method and if it is not `sendChatAction`
+    // Then we edit the payload and attach the force reply markup.
+    if (isSendMethod && isNotChatAction) {
+      payload["reply_markup"] = ForceReply().toJson();
+    }
+
+    // Return the modified payload
+    return payload;
+  }
+}
+
+void main(List<String> args) {
+  // Attach the Transformer to the bot.
+  bot.use(AutoReplier());
+
+  // Continue setting up your bot
+
+  bot.command('start', (ctx) async {
+    // User will be forced to reply to this "Hello World" message.
+    await ctx.reply("Hello World");
+  });
+
+  bot.help((ctx) async {
+    // Similarly, here as well.
+    await ctx.reply("Here to help!");
+  });
+
+  bot.start();
+}
